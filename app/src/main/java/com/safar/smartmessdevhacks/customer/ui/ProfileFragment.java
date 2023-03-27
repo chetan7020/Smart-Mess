5955765c37b85211af34c7df9cc46cbd72a79d31
yash aher <aheryash2004@gmail.com>
2023-03-28 00:03:33 +0530

    layout for edit profile

--- app/src/main/java/com/safar/smartmessdevhacks/customer/ui/ProfileFragment.java
+++ app/src/main/java/com/safar/smartmessdevhacks/customer/ui/ProfileFragment.java
@@ -1,31 +1,49 @@
 package com.safar.smartmessdevhacks.customer.ui;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.safar.smartmessdevhacks.LoginActivity;
+import com.safar.smartmessdevhacks.R;
 import com.safar.smartmessdevhacks.databinding.CustomerFragmentProfileBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileFragment extends Fragment {
 
     private CustomerFragmentProfileBinding binding;
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = CustomerFragmentProfileBinding.inflate(getLayoutInflater());
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
-        binding.btnLogout.setOnClickListener(new View.OnClickListener() {
+        setProfile();
+        binding.btnProfileLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 firebaseAuth.signOut();
@@ -34,7 +52,102 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        binding.btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                editProfile();
+            }
+        });
+
         return binding.getRoot();
     }
+
+
+
+
+    private void editProfile() {
+        Dialog dialog = new Dialog(getActivity());
+
+        dialog.setContentView(R.layout.customer_profilee_edit_layout);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        EditText etName, etPreferances, etPhoneNumber;
+        Button btnSaveChanges;
+
+        etName = dialog.findViewById(R.id.etName);
+        etPreferances = dialog.findViewById(R.id.etPreferances);
+        etPhoneNumber = dialog.findViewById(R.id.etPhoneNumber);
+        btnSaveChanges = dialog.findViewById(R.id.btnSaveChanges);
+
+        String name = binding.tvName.getText().toString().trim();
+        String preferances = binding.tvPreference.getText().toString().trim();
+        String phoneNumber = binding.tvPhoneNumber.getText().toString().trim();
+
+        etName.setText(name);
+        etPreferances.setText(preferances);
+        etPhoneNumber.setText(phoneNumber);
+        btnSaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btnSaveChanges.setEnabled(false);
+
+                String name = etName.getText().toString().trim();
+                String preferances = etPreferances.getText().toString().trim();
+                String phoneNumber = etPhoneNumber.getText().toString().trim();
+
+                Map<String, Object> data = new HashMap<>();
+                data.put("name", name);
+                data.put("item", preferances);
+                data.put("customerphone", phoneNumber);
+
+                firebaseFirestore
+                        .collection("Customer")
+                        .document(firebaseAuth.getCurrentUser().getEmail())
+                        .update(data)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getActivity(), "Profile Updated", Toast.LENGTH_SHORT).show();
+                                btnSaveChanges.setEnabled(true);
+                                dialog.cancel();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Failed to Update", Toast.LENGTH_SHORT).show();
+                                btnSaveChanges.setEnabled(true);
+                                dialog.cancel();
+                            }
+                        });
+
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void setProfile() {
+        firebaseFirestore
+                .collection("Customer")
+                .document(firebaseAuth.getCurrentUser().getEmail())
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        String name = value.getString("name");
+                        String item = value.getString("item");
+                        String customerphone = value.getString("phoneNumber");
+
+
+                        binding.tvName.setText(name);
+                        binding.tvPreference.setText(item);
+                        binding.tvPhoneNumber.setText(customerphone);
+                    }
+                });
+    }
+
+
+
 
 }
--- app/src/main/java/com/safar/smartmessdevhacks/owner/ui/ProfileFragment.java
+++ app/src/main/java/com/safar/smartmessdevhacks/owner/ui/ProfileFragment.java
@@ -1,23 +1,57 @@
 package com.safar.smartmessdevhacks.owner.ui;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.GeoPoint;
 import com.safar.smartmessdevhacks.LoginActivity;
+import com.safar.smartmessdevhacks.R;
 import com.safar.smartmessdevhacks.databinding.OwnerFragmentProfileBinding;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class ProfileFragment extends Fragment {
 
     private OwnerFragmentProfileBinding binding;
 
     private FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    private final static int REQUEST_CODE=100;
+    private EditText etLocation;
+    double lat, lang;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,7 +59,12 @@ public class ProfileFragment extends Fragment {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        binding.btnLogout.setOnClickListener(new View.OnClickListener() {
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        getProfileData();
+        binding.btnProfileLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 firebaseAuth.signOut();
@@ -34,6 +73,177 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        binding.btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name, messName, phoneNumber, messLocation, upi;
+
+                name = binding.tvName.getText().toString().trim();
+                messName = binding.tvMessName.getText().toString().trim();
+                phoneNumber = binding.tvPhoneNumber.getText().toString().trim();
+                messLocation = binding.tvLocation.getText().toString().trim();
+                upi = binding.tvUPI.getText().toString().trim();
+
+                createDialog(name, messName, phoneNumber, messLocation, upi);
+            }
+        });
+
         return binding.getRoot();
     }
+
+
+
+    private void getProfileData() {
+        Log.d("TAG", "getProfileData: "+firebaseAuth.getCurrentUser().getEmail());
+        firebaseFirestore
+                .collection("Owner")
+                .document(firebaseAuth.getCurrentUser().getEmail())
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        String name, messName, phoneNumber, messLocation, upi;
+
+                        name = value.getString("name");
+                        messName = value.getString("messname");
+                        phoneNumber = value.getString("ownerphone");
+                        messLocation = value.getString("location");
+                        upi = value.getString("upi");
+
+                        Log.d("TAG", "onEvent: "+name);
+
+                        binding.tvName.setText(name);
+                        binding.tvMessName.setText(messName);
+                        binding.tvPhoneNumber.setText(phoneNumber);
+                        binding.tvLocation.setText(messLocation);
+                        binding.tvUPI.setText(upi);
+                    }
+                });
+    }
+
+    private void createDialog(String name, String messName, String ownerphone, String messLocation, String upi) {
+        Dialog dialog = new Dialog(getActivity());
+
+        dialog.setContentView(R.layout.owner_profile_edit_layout);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        EditText etName, etMessName, etPhoneNumber, etUPI;
+        Button btnChangeLocation, btnSaveChanges;
+
+        btnSaveChanges = dialog.findViewById(R.id.btnSaveChanges);
+        btnChangeLocation = dialog.findViewById(R.id.btnChangeLocation);
+
+        etName = dialog.findViewById(R.id.etName);
+        etMessName = dialog.findViewById(R.id.etMessName);
+        etPhoneNumber = dialog.findViewById(R.id.etPhoneNumber);
+        etLocation = dialog.findViewById(R.id.etLocation);
+        etUPI = dialog.findViewById(R.id.etUPI);
+
+        etName.setText(name);
+        etMessName.setText(messName);
+        etPhoneNumber.setText(ownerphone);
+        etUPI.setText(upi);
+        etLocation.setText(messLocation);
+
+        btnChangeLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLastLocation();
+            }
+        });
+
+        btnSaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                GeoPoint geo_pointLocation = new GeoPoint(lat, lang);
+                Map<String, Object> data = new HashMap<>();
+                data.put("name", etName.getText().toString().trim());
+                data.put("messname", etMessName.getText().toString().trim());
+                data.put("ownerphone", etPhoneNumber.getText().toString().trim());
+                data.put("location", etLocation.getText().toString().trim());
+                data.put("upi", etUPI.getText().toString().trim());
+                data.put("geo_pointLocation", geo_pointLocation);
+
+                firebaseFirestore
+                        .collection("Owner")
+                        .document(firebaseAuth.getCurrentUser().getEmail())
+                        .update(data)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getActivity(), "Profile Updated", Toast.LENGTH_SHORT).show();
+                                dialog.cancel();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Failed to Update", Toast.LENGTH_SHORT).show();
+                                dialog.cancel();
+                            }
+                        });
+            }
+        });
+        dialog.show();
+    }
+    private void getLastLocation() {
+
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                            if(location!=null)
+                            {
+                                Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                                List<Address> addresses = null;
+                                try {
+                                    addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                                    etLocation.setText(addresses.get(0).getAddressLine(0));
+                                    lat = addresses.get(0).getLatitude();
+                                    lang = addresses.get(0).getLongitude();
+                                } catch (IOException e)
+                                {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }
+                    });
+        }
+        else
+        {
+            askPermission();
+        }
+
+    }
+
+    private void askPermission() {
+
+        ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE)
+        {
+            if(grantResults.length>0 &&  grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                getLastLocation();
+            }
+            else
+            {
+                Toast.makeText(getActivity(), "Please provide required Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
 }
