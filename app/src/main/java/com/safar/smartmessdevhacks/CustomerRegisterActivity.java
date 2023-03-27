e7ad4ede9b292ca89241115292fdceca81c14df1
TMR <chetandagajipatil333@gmail.com>
2023-03-27 10:45:46 +0530

    authentication files added

---
+++ app/src/main/java/com/safar/smartmessdevhacks/CustomerRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.safar.smartmessdevhacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_register);
+    }
+}
