a0e864a7bf74ecd14cf896a37fd7d24d0e74f133
TMR <chetandagajipatil333@gmail.com>
2023-03-30 16:08:37 +0530

    ProfileFragment.java code deleted

--- app/src/main/java/com/safar/smartmessdevhacks/customer/ui/ProfileFragment.java
+++ app/src/main/java/com/safar/smartmessdevhacks/customer/ui/ProfileFragment.java
@@ -32,114 +32,11 @@ public class ProfileFragment extends Fragment {
 
     private CustomerFragmentProfileBinding binding;
 
-    private FirebaseAuth firebaseAuth;
-    private FirebaseFirestore firebaseFirestore;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = CustomerFragmentProfileBinding.inflate(getLayoutInflater());
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
-
-        setProfile();
-        binding.btnProfileLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firebaseAuth.signOut();
-                startActivity(new Intent(getActivity(), LoginActivity.class));
-                getActivity().finish();
-            }
-        });
-
-        binding.btnEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                editProfile();
-            }
-        });
-
         return binding.getRoot();
     }
-
-
-
-
-    private void editProfile() {
-        Dialog dialog = new Dialog(getActivity());
-
-        dialog.setContentView(R.layout.customer_profilee_edit_layout);
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        EditText etName, etPreferances, etPhoneNumber;
-        Button btnSaveChanges;
-
-        etName = dialog.findViewById(R.id.etName);
-        etPreferances = dialog.findViewById(R.id.etPreferances);
-        etPhoneNumber = dialog.findViewById(R.id.etPhoneNumber);
-        btnSaveChanges = dialog.findViewById(R.id.btnSaveChanges);
-
-        String name = binding.tvName.getText().toString().trim();
-        String preferances = binding.tvPreference.getText().toString().trim();
-        String phoneNumber = binding.tvPhoneNumber.getText().toString().trim();
-
-        etName.setText(name);
-        etPreferances.setText(preferances);
-        etPhoneNumber.setText(phoneNumber);
-        btnSaveChanges.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                btnSaveChanges.setEnabled(false);
-
-                String name = etName.getText().toString().trim();
-                String preferances = etPreferances.getText().toString().trim();
-                String phoneNumber = etPhoneNumber.getText().toString().trim();
-
-                Map<String, Object> data = new HashMap<>();
-                data.put("name", name);
-                data.put("item", preferances);
-                data.put("customerphone", phoneNumber);
-
-                firebaseFirestore
-                        .collection("Customer")
-                        .document(firebaseAuth.getCurrentUser().getEmail())
-                        .update(data)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Toast.makeText(getActivity(), "Profile Updated", Toast.LENGTH_SHORT).show();
-                                btnSaveChanges.setEnabled(true);
-                                dialog.cancel();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(getActivity(), "Failed to Update", Toast.LENGTH_SHORT).show();
-                                btnSaveChanges.setEnabled(true);
-                                dialog.cancel();
-                            }
-                        });
-
-            }
-        });
-
-        dialog.show();
-    }
-
-    private void setProfile() {
-        firebaseFirestore
-                .collection("Customer")
-                .document(firebaseAuth.getCurrentUser().getEmail())
-                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                    }
-                });
-    }
-
-
-
 
 }
